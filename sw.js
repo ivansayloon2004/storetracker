857d1ce5890678969e8476e247541cd282a1b6dc
ivansayloon <ivnasayloon20@gmail.com>
2026-03-29 12:33:17 +0800

    Bump app version for profit calendar rollout

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "tindahan-tracker-shell-20260329k";
+const CACHE_NAME = "tindahan-tracker-shell-20260329l";
 const APP_SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=20260329k",
-  "/script.js?v=20260329k",
-  "/manifest.webmanifest?v=20260329k",
+  "/styles.css?v=20260329l",
+  "/script.js?v=20260329l",
+  "/manifest.webmanifest?v=20260329l",
   "/icons/icon-192.png",
   "/icons/icon-512.png",
   "/icons/apple-touch-icon.png",
